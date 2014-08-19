eb298318ac14043b59b8bbe7087e854160947889
winfirm <panxuewen@gmail.com>
2014-08-19 23:11:36 +0800

    update comment of QuickResourceLoader.java

--- AndroidQuickLibrary/src/com/quick/library/QuickResourceLoader.java
+++ AndroidQuickLibrary/src/com/quick/library/QuickResourceLoader.java
@@ -3,15 +3,41 @@ package com.quick.library;
 import com.android.http.LoadControler;
 import com.android.http.RequestManager.RequestListener;
 
-/** 
- * description：   QuickResourceLoader
- * @author  steven-pan
- * @date    2014年8月19日
+/**
+ * description： QuickResourceLoader
+ * 
+ * @author steven-pan
+ * @date 2014年8月19日
  * @version 1.0
  */
 public interface QuickResourceLoader {
+
+	/**
+	 * load String resource from network by GET method
+	 * 
+	 * @param url
+	 *            tartget url
+	 * @param requestListener
+	 *            request listener for callback
+	 * @param actionId
+	 *            request id
+	 * @return
+	 */
+	public LoadControler get(String url, RequestListener requestListener, int actionId);
+
+	/**
+	 * load String resource from network by POST method
+	 * 
+	 * @param url
+	 *            tartget url
+	 * @param data
+	 *            postBody
+	 * @param requestListener
+	 *            request listener for callback
+	 * @param actionId
+	 *            request id
+	 * @return
+	 */
+	public LoadControler post(String url, String data, RequestListener requestListener, int actionId);
 	
-	public LoadControler get(String url, RequestListener requestListener, int actionId) ;
-	
-	public LoadControler post(String url, String data, RequestListener requestListener, int actionId) ;
 }
